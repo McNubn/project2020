850e94c72e6d08a03a2289d7629a723eade65795
Brian Doheny <brian.doheny@shopify.com>
2020-03-15 09:07:25 +0000

    Speal leng vs sepal width scatter plot is working

--- irisAnalysis.py
+++ irisAnalysis.py
@@ -1,16 +1,28 @@
 # Brian Doheny
+# Three main parts:
+   # 1. Research the data set online and write a summary about it in the README (using markdown for formatting).
+  #  2. Download the data set and add it to the repo. - Done
+  #  3. Write a program called analysis.py that:
+      #  a. outputs a summary of each file to a single text file,
+      #  b. saves a histogram of each variable to png files,
+      #  c. outputs a scatter plot of each pair of variables.
+
 # Minimum Viable Project:
     # Standard repo that contains a README, a Python script, a summary text file and images. 
     # The README should contain a summary of the data set and my investigations into it.
     # It should also clearly document how to run the Python code and what the code does.
     # Remember to referece everything - e.g. where I found a code snippet.
 
-# Week 1 (ending Sunday March 15th) - Setup
-        # Repo is set up - done.
-        # Quality version of the data set is found, and added to repo - done.
-        # Started getting data set into Pandas data frames, ready for plotting and manipulation.
+#     Week 2 (ending Sunday March 22nd) - Plotting
+       # Histograms and Scatterplots are setup and working. 
+       # Begin formatting these to make them clear to read.
+       # Start pushing some analyses to the text file.
 
 import pandas as pd
+from matplotlib import pyplot as plt
 
-df = pd.read_csv("iris_csv.csv")
-print(df)
+# Save the csv to a pandas data frame. I'm setting the index to be the class column.
+df = pd.read_csv("iris_csv.csv", index_col = "class")
+
+plt.scatter(df['sepallength'],df['sepalwidth'])
+plt.show()

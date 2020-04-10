8a661564ec9e2bee3bffe5bfd56b8a3872d8dfd6
Brian Doheny <brian.doheny@shopify.com>
2020-04-10 08:37:12 +0100

    Added a boxplot for now.

--- irisAnalysis.py
+++ irisAnalysis.py
@@ -25,6 +25,7 @@ csv = 'iris_csv.csv'
 
 # Save the csv to a pandas data frame. I'm setting the index to be the class column.
 df = pd.read_csv(csv, index_col = "class")
+
 # Got the idea to iterate through column names from https://www.marsja.se/how-to-get-the-column-names-from-a-pandas-dataframe-print-and-list/#3_Get_Column_Names_by_Iterating_of_the_Columns
 # Column names without the space looks odd. Figure if I can get past the "l" in Sepal and Petal, and split it into 2 strings with a space in the middle, it'll look neater.
 # Since I'll need this again later, I'm turning it into a function called nameFormat.
@@ -98,3 +99,8 @@ try:
 except:
     pass
 
+#box plots - guide found http://www.datasciencemadesimple.com/box-plot-in-python/
+box_plot_data = [df['sepallength'],df['sepalwidth'],df['petallength'],df['petalwidth']]
+plt.boxplot(box_plot_data,patch_artist = True, labels = ['Sepal Length', 'Sepal Width', 'Petal Length', 'Petal Width'])
+plt.savefig("boxplot.png")
+plt.clf()
